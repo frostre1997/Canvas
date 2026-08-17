5322a49f5f3371a18d491b626fa2632d8b912cba
Frostre_ <n9043395@gmail.com>
2026-08-17 08:16:14 +0200

    Downgrade versionCode and versionName in build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.canvas.android.app"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1001
-        versionName = "0.100.1"
+        versionCode = 1000
+        versionName = "0.100.0"
     }
 
     buildFeatures {
